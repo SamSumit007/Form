05d007df1bd63a87ccdd791225cc1188cb893b9e
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-04-18 16:53:34 +0530

    form

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import { AddStudent } from "./components/AddStudent";
+import { ShowStudents } from "./components/ShowStudents";
+import {useState} from "react"
+import './App.css';
+function App() {
+  const [state, setState] = useState(true)
+
+
+  const changeFn = () => {
+    setState(!state)
+  }
+  return (
+    <div className="App">
+      <button onClick={changeFn} className="togglebtn">{state ? 'Add' : 'Show'} Student</button>
+      {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
+      {/* make sure the table is shown initially, do not show form initially */}
+      {/* make sure to show either of them do not both together */}
+      {state? <ShowStudents/>: <AddStudent/>}
+    </div>
+  );
+}
+
+export default App;
